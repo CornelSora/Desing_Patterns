ec939c64bfa7467324886c501dc1d223fd660318
CornelSora <cornelsora@gmail.com>
2018-12-28 23:33:43 +0200

    advise

--- state/src/Main.java
+++ state/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Rezervare rezervare = new Rezervare(32);
-        StareNeplatita stareNeplatita = new StareNeplatita();
-        stareNeplatita.doAction(rezervare);
-
+        StareNeplatita stareNeplatita = new StareNeplatita(); // I recommend to use the class Stare here
+        stareNeplatita.doAction(rezervare); // I recommend to set the state on rezervare: rezervare.SetStare and then rezervare.getStare().doAction 
+                                            // or stare.doAction, but Stare should be the abstract class.
         StarePlatita starePlatita = new StarePlatita();
         starePlatita.doAction(rezervare);
 
